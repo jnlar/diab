0312242b5cdda13e3b6434062f9e0542cd704e6d
jnlar <jon357@pm.me>
2022-01-11 08:57:24 +1100

    Added SVGs

---
+++ assets/icons/svg.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export function PlusCircle(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className={props.className} viewBox="0 0 20 20" fill="currentColor">
+      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />
+    </svg>
+  )
+}
+
+export function Foo(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" className={props.className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+    </svg>
+  )
+}
